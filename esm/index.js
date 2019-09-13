a37808b4e04c7996f715bae6dcce6a9ddce753b8
thipages <thipages@gmail.com>
2019-09-13 14:46:25 +0200

    add refresh delay

--- esm/index.js
+++ esm/index.js
@@ -1,5 +1,23 @@
+let running=false;
+const refresh=(delay,c)=> {
+    if (window.requestAnimationFrame) {
+        window.requestAnimationFrame(()=>c());
+    } else {
+        setTimeout(()=>c(), delay);
+    }
+};
+const call=(c,inner)=> {
+    c(winDimension(inner));
+    running=false;
+};
 const d=(inner)=>inner
     ? ({w:window.innerWidth,h:window.innerHeight})
     : ({w:window.outerWidth,h:window.outerHeight});
 export const winDimension=(inner=true)=> d(inner);
-export const winResize=(c, inner=true)=> window.onresize=()=>c(winDimension(inner));
+export const winResize=(c, inner=true,delay=true)=> window.onresize=()=>{
+    // https://github.com/freaksauce/window-resize/
+    if (!running) {
+        running = true;
+        if (delay) refresh(60,()=>call(c,inner)); else call(c,inner);
+    }
+};
