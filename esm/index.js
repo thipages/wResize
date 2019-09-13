15621cb63c4617dc5d5f5dccc5fe62c402053476
thipages <thipages@gmail.com>
2019-09-13 14:26:54 +0200

    rename exports for clarity

--- esm/index.js
+++ esm/index.js
@@ -1,5 +1,5 @@
-const d1=(inner)=>inner
+const d=(inner)=>inner
     ? ({w:window.innerWidth,h:window.innerHeight})
     : ({w:window.outerWidth,h:window.outerHeight});
-export const wDimension=(inner=true)=> d1(inner);
-export const wResize=(c,inner=true)=> window.onresize=()=>c(wDimension(inner));
+export const winDimension=(inner=true)=> d(inner);
+export const winResize=(c, inner=true)=> window.onresize=()=>c(winDimension(inner));
